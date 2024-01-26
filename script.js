f57a883075e0b0c52c06de69526bbb4d14f2d523
dishag07 <dishag422@gmail.com>
2024-01-26 14:53:03 -0500

    finish

---
+++ script.js
@@ -0,0 +1,33 @@
+function firstAssistant() {
+    document.getElementById('words').innerText= ' ★ On February 28, 1911, President William Howard Taft made a groundbreaking appointment by nominating William H. Lewis, an African American from Massachusetts, to the position of Assistant Attorney General of the United States. This nomination was significant for several reasons, notably for being one of the highest federal appointments of an African American up to that time. Lewiss appointment was a notable step in the long journey towards racial equality in the United States, coming at a time when African Americans faced widespread discrimination and segregation under Jim Crow laws, particularly in the South.'
+    document.getElementById('me').src = '/images/president.jpg' 
+    document.getElementById('meee').src = '/images/will.jpg' 
+
+}
+
+function doubleHelix() {
+    document.getElementById('words').innerText= ' ★ On February 28, 1953, James Watson and Francis Crick made a groundbreaking scientific discovery, they determined the structure of DNA, identifying it as a double helix. This discovery was a monumental achievement in the field of molecular biology and genetics, fundamentally changing our understanding of how genetic information is stored and transmitted in living organisms.'
+    document.getElementById('me').src = '/images/science.webp' 
+    document.getElementById('meee').src = '/images/alsoscience.webp' 
+}
+
+function londonBus() {
+    document.getElementById('words').innerText= ' ★ On February 28, 1975, a tragic and catastrophic event occurred in Londons underground subway system, known as the Tube, marking one of the darkest days in its history. A Northern City Line train failed to stop at Moorgate station, one of the networks busiest stations, and crashed into the dead-end tunnel beyond the platform. This horrific accident resulted in the deaths of 43 people, including the trains driver, and injured 74 others, making it the worst disaster in the history of the London Underground up to that time.'
+    document.getElementById('me').src = '/images/bus.jpg' 
+    document.getElementById('meee').src = '/images/bridgeasf.jpg' 
+
+}
+
+function princessDiana() {
+    document.getElementById('words').innerText= ' ★ On February 28, 1996, a significant moment in British royal history occurred when Princess Diana agreed to divorce Prince Charles. This decision marked the end of a marriage that had been under intense public scrutiny for many years and was a major event not only for the British royal family but also for the public both in the United Kingdom and around the world.'
+    document.getElementById('me').src = '/images/princess.jpg' 
+    document.getElementById('meee').src = '/images/divorce.jpg' 
+
+}
+
+function covidRise() {
+    document.getElementById('words').innerText= ' ★ On February 28, 2020, the world was grappling with the escalating situation of the COVID-19 pandemic. The number of countries reporting cases of the novel coronavirus, SARS-CoV-2, had risen to nearly 60, signaling a significant and worrying spread of the virus beyond its initial outbreak center in Wuhan, China. This marked a critical juncture in the global health crisis, as the disease was not just a regional issue anymore but a worldwide concern, affecting continents and numerous countries with varying degrees of severity.'
+    document.getElementById('me').src = '/images/sick.jpg' 
+    document.getElementById('meee').src = '/images/GRAPH.avif' 
+
+}
